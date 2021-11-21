e20bf69db691fad1d135b548a1a86f15322e2869
Ana Martinez <ana.jane.mm94@gmail.com>
2021-11-21 22:00:05 +0100

    privado

--- Futuro/src/futuro/Futuro.java
+++ Futuro/src/futuro/Futuro.java
@@ -22,7 +22,7 @@ public class Futuro {
         System.out.println("Estudiante " + args[0] + " : ");
         
     }
-    public static String veredicto(String valor1, String valor2) {
+    private static String veredicto(String valor1, String valor2) {
     
         
 if (valor1.equals(valor2)) {
